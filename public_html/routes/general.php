7990c46e29507e61d3d9904caba266f2d8e3805c
liam-wakely <wakelyliam@gmail.com>
2023-12-12 13:29:30 +0000

    small tweaks

--- public_html/routes/general.php
+++ public_html/routes/general.php
@@ -1,18 +1,14 @@
 <?php
 use Classes\Example;
 
-$app->redirect('/', '/home');
+$app->redirect('/home', '/');
 
 $app->get('/example', function ($request, $response, $args){
-	$example = Example::getExample();
-	$data = ["example" => $example];
+	$data = ["example" => Example::getExample()];
     return $this->get('view')->render($response, 'example.twig', $data);
 })->setName('example');
 
-$app->get('/home', function ($request, $response, $args){
-
-	//calling DB:
-	//DBWrapper::fetch_all("SELECT * FROM test_table");
+$app->get('/', function ($request, $response, $args){
 	$data = [];
     return $this->get('view')->render($response, 'home.twig', $data);
 })->setName('home');
